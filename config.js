fe99256f3375e5f5c28e8fe38afd6a628fd33cf5
Chris Galardi <ixplode@gmail.com>
2014-07-09 16:39:50 -0400

    it works :D

--- config.js
+++ config.js
@@ -8,13 +8,15 @@ var prefix = '//cdnjs.cloudflare.com/ajax/libs/';
 var config = {
 
 	shim: {
-		jquery: { exports: 'jQuery'}
+		jquery: { exports: 'jQuery'},
+		'dagre-d3': { exports: 'dagreD3' },
+		'dot-lib': { exports: 'graphlibDot' }
 	},
 
 	paths: {
 		jquery: prefix + 'jquery/2.1.1/jquery.min',
-		d3: 'http://d3js.org/d3.v3.min.js',
-		'dagre-d3': 'http://cpettitt.github.io/project/dagre-d3/latest/dagre-d3.min.js',
+		'dot-lib': 'http://cpettitt.github.io/project/graphlib-dot/latest/graphlib-dot.min',
+		'dagre-d3': 'http://cpettitt.github.io/project/dagre-d3/latest/dagre-d3',
 		ace: 'ace/lib/ace'
 	}
 };
--- main.js
+++ main.js
@@ -6,19 +6,60 @@ define(function(require)
 
 var $ = require('jquery');
 var ace = require('ace/ace');
+var dot = require('dot-lib');
+var d3 = window.d3;
+var dagre = require('dagre-d3');
 
-function LiveDOT()
+function transition(selection)
 {
-
+	return selection.transition().duration(500);
 }
 
 
-LiveDOT.prototype.init = function(elt)
+
+function LiveDOT()
 {
-	var editor = ace.edit(elt);
-	editor.setTheme('ace/theme/monokai');
-	editor.getSession().setMode('ace/mode/dot');
+	this.svgElement = null;
+	this.editor = null;
+}
+
+
+LiveDOT.prototype.init = function(editorElement, viewElement)
+{
+	this.editor = ace.edit(editorElement);
+	this.editor.setTheme('ace/theme/monokai');
+	this.editor.getSession().setMode('ace/mode/dot');
+	this.editor.on('change', this.onChange.bind(this));
+
+	this.svgElement = $(viewElement).children('svg')[0];
+
 	return this;
+};
+
+
+LiveDOT.prototype.onChange = function(diff)
+{
+
+	var raw = this.editor.getValue();
+
+	var parseOutput = null;
+
+	try {
+		parseOutput = dot.parse(raw);
+	} catch (e) {
+
+	}
+
+	if (!parseOutput)
+	{
+		return;
+	}
+
+	var svg = d3.select(this.svgElement);
+	var renderer = new dagre.Renderer();
+
+	renderer.transition(transition);
+	var layout = renderer.run(parseOutput, d3.select('svg g'));
 };
 
 
